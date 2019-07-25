5b6519b8b3766787521cc7dbb773ce1523a206a9
양동권 <ydk9557819@naver.com>
2019-07-25 15:46:07 +0900

    [+] Edit adminPage member

--- src/views/AdminPage.vue
+++ src/views/AdminPage.vue
@@ -7,6 +7,7 @@
       </div>
     </ImgBanner>
 
+    <!-- Content -->
     <v-container>
       <v-card flat>
         <v-tabs vertical>
@@ -18,15 +19,42 @@
           <!-- member -->
           <v-tab-item>
             <v-card flat>
-              <v-card-text>
-                <v-data-table
-                  :headers="headers"
-                  :items="users"
-                  :items-per-page="5"
-                  class="elevation-1"
-                >
-                </v-data-table>
-              </v-card-text>
+              <v-card-title>
+                <p>
+                  Members
+                </p>
+                <v-spacer></v-spacer>
+                <v-text-field
+                  v-model="search"
+                  append-icon="search"
+                  label="Search"
+                  single-line
+                  hide-details
+                  color="#00adb5"
+                ></v-text-field>
+              </v-card-title>
+              <v-data-table
+                :headers="headers"
+                :items="users"
+                class="elevation-1"
+                :search="search"
+                prev-icon="fa-caret-left"
+                next-icon="fa-caret-right"
+              >
+                <template v-slot:items="props">
+                  <td>{{ props.item.umail }}</td>
+                  <td>{{ props.item.upasswd }}</td>
+                  <td v-if="props.item.uauth == 0">
+                    <span id="u_guest">guest</span>
+                  </td>
+                  <td v-if="props.item.uauth == 1">
+                    <span id="u_member">member</span>
+                  </td>
+                  <td v-if="props.item.uauth == 2">
+                    <span id="u_admin">admin</span>
+                  </td>
+                </template>
+              </v-data-table>
             </v-card>
           </v-tab-item>
 
@@ -64,34 +92,34 @@
 
 <script>
 import ImgBanner from "../components/ImgBanner";
-import axios from "axios";
+import Server from "../services/Server.js";
+const SERVER_URL = "http://localhost:5000";
 
 export default {
   data() {
     return {
+      search: "",
       headers: [
-        {
-          text: "User Email",
-          align: "left",
-          sortable: false,
-          value: "email"
-        },
-        { text: "User Password", value: "password" },
-        { text: "User Grade", value: "grade" },
-        { text: "Change Grade", value: "chang" }
+        { text: "Email", sortable: false, value: "uemail" },
+        { text: "PassWord", sortable: false, value: "upasswd" },
+        { text: "Grade", sortable: false, value: "uauth" }
       ],
-      testData: []
+      users: []
     };
   },
   components: {
     ImgBanner
   },
   mounted() {
-    this.getPosts();
+    this.getUsers();
   },
-  method: {
-    async getPosts() {
-      await axios.get("http://localhost:5000/api/admin");
+  methods: {
+    async getUsers() {
+      await Server(SERVER_URL)
+        .get("/api/users")
+        .then(res => {
+          this.users = res["data"];
+        });
     }
   }
 };
