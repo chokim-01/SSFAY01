d928d8d1e2975a22d68e0927ddb2724c7f63bb90
default <chokim159@naver.com>
2019-07-30 13:34:46 +0900

    S1P3113010-49 [+] Edit serve -> build

--- public/service_worker.js
+++ public/service_worker.js
@@ -1,23 +1,22 @@
-var CACHE_NAME = 'v6';
+var CACHE_NAME = "v6";
 var urlsToCache = [
-  '/',
-  '/manifest.json',
-  '/app.js',
-  '/css/common.css',
-  '/img/icons/favicon-16x16.png',
-  'http://localhost:5000/api/get/portfolios',
-  'http://localhost:5000/api/get/posts',
-  'https://lab.ssafy.com/api/v4/projects/',
-  'https://lab.ssafy.com/api/v4/users/k3y6reak/projects'
+  "/",
+  "/manifest.json",
 
+  "/img/icons/favicon-16x16.png",
+  "http://localhost:5000/api/get/portfolios",
+  "http://localhost:5000/api/get/posts",
+  "https://source.unsplash.com/random ",
+  "http://localhost:5000/img/about_bg.32b6cf06.jpg",
+  "http://localhost:5000/fonts/fontawesome-webfont.af7ae505.woff2"
 ];
 // ServiceWorker install
-self.addEventListener('install', function(event) {
+self.addEventListener("install", function(event) {
   // Perform install steps
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
-        console.log('Opened cache');
+        console.log("Opened cache");
         return cache.addAll(urlsToCache);
       }).catch(function(err){
         console.log(err);
@@ -25,8 +24,7 @@ self.addEventListener('install', function(event) {
   );
 });
 // ServiceWorker Fetch
-self.addEventListener('fetch',event => {
-  console.log('Fetch ' +event.request.url);
+self.addEventListener("fetch",event => {
   event.respondWith(
     caches.match(event.request).then(function(response) {
       return response || fetch(event.request);
@@ -35,13 +33,13 @@ self.addEventListener('fetch',event => {
 })
 
 //Remove before installed ServiceWorker
-self.addEventListener('activate', event => {
-  console.log('Activate');
+self.addEventListener("activate", event => {
+  console.log("Activate");
   event.waitUntil(
     caches.keys().then(keyList => {
       return Promise.all(keyList.map(key => {
         if (key !== CACHE_NAME) {
-          console.log('Remove old cache' + key);
+          console.log("Remove old cache" + key);
           return caches.delete(key);
         }
       }));
